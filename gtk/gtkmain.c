ab94aa452337c8db24c2366c139c684ae2625c6c
stefan11111 <stefan11111@shitposting.expert>
2024-04-27 16:17:09 +0300

    fix some deprecation warnings

--- gtk/gtkmain.c
+++ gtk/gtkmain.c
@@ -744,9 +744,6 @@ do_post_parse_initialization (int    *argc,
       g_warning ("Whoever translated default:LTR did so wrongly.\n");
   }
 
-  /* do what the call to gtk_type_init() used to do */
-  g_type_init ();
-
   _gtk_accel_map_init ();
   _gtk_rc_init ();
 
