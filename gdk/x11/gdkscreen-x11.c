7bc15fb0ea676a2847d765dd3779012f7ef5a9b6
stefan11111 <stefan11111@shitposting.expert>
2024-05-02 20:27:15 +0300

    fix an error

--- gdk/x11/gdkscreen-x11.c
+++ gdk/x11/gdkscreen-x11.c
@@ -1493,7 +1493,8 @@ gdk_screen_get_window_stack (GdkScreen *screen)
 
           for (i = 0; i < nitems_return; i++)
             {
-              if(GdkWindow *win = gdk_window_foreign_new_for_display (screen_x11->display, (GdkNativeWindow)stack[i])) {
+              GdkWindow *win;
+              if(win = gdk_window_foreign_new_for_display (screen_x11->display, (GdkNativeWindow)stack[i])) {
                 ret = g_list_append (ret, win);
               }
             }
